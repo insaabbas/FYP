942c9be636ab6b89ee2594d3df560aee0426acd0
Insa Abbas <insaabbas675@gmail.com>
2026-06-12 00:04:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Clean setup for running locally on your PC
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/FYP/',
 })
